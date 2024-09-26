9b833f2b857d178be1b4e766a1864a5763853815
Miiihawk <131926009+Miiihawk@users.noreply.github.com>
2024-09-26 23:07:20 +0800

    Added features where its able to show amount and the account no. in confirmation and success pages

--- lib/transfer.dart
+++ lib/transfer.dart
@@ -1,4 +1,3 @@
-import 'package:cs319_dart_playground/main.dart';
 import 'package:flutter/material.dart';
 
 class Transfer extends StatefulWidget {
@@ -9,11 +8,23 @@ class Transfer extends StatefulWidget {
 }
 
 class _TransferState extends State<Transfer> {
+  // Define TextEditingController for the Account and Amount fields
+  final TextEditingController _accountController = TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Dispose the controllers when not needed to free up resources
+    _accountController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('Transfer Money'),
       ),
       body: Center(
         child: Padding(
@@ -37,28 +48,32 @@ class _TransferState extends State<Transfer> {
                     ),
                   ),
                   const SizedBox(height: 10),
-                  Text(
-                    'Enter User ID:',
-                    style: const TextStyle(
+                  const Text(
+                    'Enter Account No.:',
+                    style: TextStyle(
                       fontSize: 16,
                     ),
                   ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 10),
+                  // Use the account controller
                   TextField(
+                    controller: _accountController,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                     ),
                     keyboardType: TextInputType.number,
                   ),
-                  const SizedBox(height: 10),
-                  Text(
+                  const SizedBox(height: 20),
+                  const Text(
                     'Enter Amount to Transfer:',
-                    style: const TextStyle(
+                    style: TextStyle(
                       fontSize: 16,
                     ),
                   ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 10),
+                  // Use the amount controller
                   TextField(
+                    controller: _amountController,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                     ),
@@ -66,14 +81,21 @@ class _TransferState extends State<Transfer> {
                   ),
                   const SizedBox(height: 20),
                   ElevatedButton(
-                    onPressed: (){
+                    onPressed: () {
+                      // Retrieve input values
+                      String inputAccount = _accountController.text;
+                      String inputAmount = _amountController.text;
 
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => const Confirmation()),
+                        MaterialPageRoute(
+                            builder: (context) => Confirmation(
+                              account: inputAccount,
+                              amount: inputAmount,
+                            )),
                       );
                     },
-                    child: const Text('Procees '),
+                    child: const Text('Proceed'),
                   ),
                 ],
               ),
@@ -85,15 +107,16 @@ class _TransferState extends State<Transfer> {
   }
 }
 
+class Confirmation extends StatelessWidget {
 
-class Confirmation extends StatefulWidget {
-  const Confirmation({super.key});
+  final String account;
+  final String amount;
 
-  @override
-  _ConfirmationState createState() => _ConfirmationState();
-}
-
-class _ConfirmationState extends State<Confirmation> {
+  const Confirmation({
+    super.key,
+    required this.account,
+    required this.amount,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -102,14 +125,11 @@ class _ConfirmationState extends State<Confirmation> {
         title: const Text('Confirmation'),
       ),
       body: Center(
-
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-
               Card(
                 elevation: 5,
                 shape: const RoundedRectangleBorder(
@@ -117,35 +137,37 @@ class _ConfirmationState extends State<Confirmation> {
                 ),
                 color: const Color.fromARGB(255, 255, 255, 255),
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  padding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                   child: Column(
                     children: [
-                      Text('You are about to send...'),
-
+                      Text('You are about to send Php$amount to Account No. #$account'),
                     ],
                   ),
                 ),
               ),
-
               const Text(
                 'Are you sure you want to proceed?',
                 style: TextStyle(fontSize: 18),
               ),
               const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: (){
-
+                onPressed: () {
+                  // Navigate to the Success page with account and amount details
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const Success()),
+                    MaterialPageRoute(builder: (context) => Success(
+                      account: account,
+                      amount: amount,
+                    )),
                   );
                 },
                 child: const Text('Confirm'),
               ),
               const SizedBox(height: 10),
               ElevatedButton(
-                onPressed: (){
-
+                onPressed: () {
+                  // Navigate back to the Transfer page
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => const Transfer()),
@@ -161,16 +183,16 @@ class _ConfirmationState extends State<Confirmation> {
   }
 }
 
+class Success extends StatelessWidget {
 
+  final String account;
+  final String amount;
 
-class Success extends StatefulWidget {
-  const Success({super.key});
-
-  @override
-  _SuccessState createState() => _SuccessState();
-}
-
-class _SuccessState extends State<Success> {
+  const Success({
+    super.key,
+    required this.account,
+    required this.amount,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -179,14 +201,11 @@ class _SuccessState extends State<Success> {
         title: const Text(''),
       ),
       body: Center(
-
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-
               Card(
                 elevation: 5,
                 shape: const RoundedRectangleBorder(
@@ -194,25 +213,23 @@ class _SuccessState extends State<Success> {
                 ),
                 color: const Color.fromARGB(255, 255, 255, 255),
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  padding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                   child: Column(
                     children: [
                       Text('STATUS: SUCCESS'),
-                      Text('TRANSACTION DATE:...'),
+                      Text('TRANSACTION DATE: ...'),
                       Text('TRANSACTION TYPE: Transfer Money'),
-                      Text('MESSAGE: Transferred... to ...'),
+                      Text('MESSAGE: Transferred Php $amount to Account No. $account'),
                       Text('OLD BALANCE:...'),
                       Text('NEW BALANCE:...'),
-
                     ],
                   ),
                 ),
               ),
-
               const SizedBox(height: 10),
               ElevatedButton(
-                onPressed: (){
-
+                onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => const Transfer()),
@@ -227,4 +244,3 @@ class _SuccessState extends State<Success> {
     );
   }
 }
-
