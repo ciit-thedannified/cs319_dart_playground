92d22c3c344fc7b037063d7345cc79f62e3ed1eb
ciit-kojiescolar <129504627+ciit-kojiescolar@users.noreply.github.com>
2024-10-12 18:32:23 +0800

    Fixed card.dart & home.dart
    Added images for the cards and also applied a search button feature for to get specifics.

--- lib/card.dart
+++ lib/card.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
-// To do's 
+// To do's
 // Add image based on the filipino food on Home.dart (Still can be changed if you want)
 // Redesign if ever gusto mo
 
-class ProductCard extends StatefulWidget{
+class ProductCard extends StatefulWidget {
   final String title;
   final String price;
+  final String imageURL; // For it to hold image url in the card
   final VoidCallback onTap;
 
   const ProductCard({
     Key? key,
     required this.title,
     required this.price,
+    required this.imageURL, //Pass the imageURl to the constructor
     required this.onTap,
   }) : super(key: key);
 
@@ -20,9 +22,9 @@ class ProductCard extends StatefulWidget{
   _ProductCardState createState() => _ProductCardState();
 }
 
-class _ProductCardState extends State<ProductCard>{
+class _ProductCardState extends State<ProductCard> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return GestureDetector(
       onTap: widget.onTap,
       child: Container(
@@ -31,9 +33,9 @@ class _ProductCardState extends State<ProductCard>{
         margin: const EdgeInsets.all(8.0),
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors:[Colors.green, const Color.fromARGB(255, 56, 142, 60)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter),
+              colors: [Colors.green, const Color.fromARGB(255, 56, 142, 60)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter),
           borderRadius: BorderRadius.circular(10),
           boxShadow: [
             BoxShadow(
@@ -48,34 +50,45 @@ class _ProductCardState extends State<ProductCard>{
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            //Image Container
             Expanded(
-            child: Container(
-              margin: EdgeInsets.fromLTRB(10, 10, 10, 6),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.green[400],
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10)),
+                child: Image.asset(
+                  widget.imageURL,
+                  width: double.infinity,
+                  height: 120,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, StackTrace) {
+                    return Container(
+                      color: Colors.grey,
+                      child: const Icon(Icons.broken_image, size: 50),
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10, 6, 8, 8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children:[
-                Text(
-                  widget.title,
-                  style:TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 6, 8, 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  widget.price,
-                  style:TextStyle(
-                    fontSize: 14,
-                    color: Colors.white,
+                  SizedBox(height: 5),
+                  Text(
+                    widget.price,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
                     ),
                   ),
                 ],
--- lib/home.dart
+++ lib/home.dart
@@ -1,74 +1,98 @@
 import 'package:flutter/material.dart';
-import 'card.dart'; 
+import 'card.dart';
 
-// To do's 
-//Search Bar 
-
-class HomePage extends StatefulWidget{
-  const HomePage({Key? key}) : super(key:key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
-  final List<Map<String,String>> _products = [
-    {"title": "Adobo", "price": "\$100"},
-    {"title": "Sinigang na Baboy", "price": "\$200"},
+class _HomePageState extends State<HomePage> {
+  final List<Map<String, String>> _products = [
+    {"title": "Adobo", "price": "\$100","image":"./images/Adobo.jpg"},
+    {"title": "Sinigang na Baboy", "price": "\$200","image":"./images/sinigang-baboy.jpg"},
     {"title": "Menudo", "price": "\$300"},
     {"title": "Sisig", "price": "\$400"},
     {"title": "Kare-Kare", "price": "\$500"},
     {"title": "Lechon", "price": "\$600"},
     {"title": "Example", "price": "\$700"},
-
   ];
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text("Home"),
-      titleTextStyle: TextStyle(fontSize: 22),
-      centerTitle: true,
-    ),
-    body: Column(
-      children: [ 
-        Padding(
-          padding: const EdgeInsets.fromLTRB(22, 8, 22, 8),
-          child: TextField( 
-            decoration: InputDecoration(
-              border: OutlineInputBorder( 
-                borderRadius: BorderRadius.circular(10),
+  List<Map<String, String>> _filteredProducts = [];
+
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredProducts = _products;
+
+    _searchController.addListener(_filterProducts);
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _filterProducts() {
+    String query = _searchController.text.toLowerCase();
+    setState(() {
+      _filteredProducts = _products.where((product) {
+        return product["title"]!.toLowerCase().contains(query);
+      }).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        titleTextStyle: TextStyle(fontSize: 22),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(22, 8, 22, 8),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Search',
               ),
-              prefixIcon: Icon(Icons.search),
-              hintText: 'Search',
             ),
           ),
-        ),
-
-        Expanded(
-          child: GridView.builder( 
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              childAspectRatio: 0.75,
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 0.75,
               ),
-             itemCount: _products.length,
-             itemBuilder: (context, index){
-            return ProductCard(
-              title: _products[index]['title'] ?? 'No title',
-              price: _products[index]['price'] ?? 'No Title',
-              onTap: () {
-                print('Tapped on ${_products[index]['title']}');
-                },
-              );
-            },
+              itemCount: _filteredProducts.length,
+              itemBuilder: (context, index) {
+                return ProductCard(
+                  title: _filteredProducts[index]['title'] ?? 'No title',
+                  price: _filteredProducts[index]['price'] ?? 'No Title',
+                  imageURL: _filteredProducts[index]['image'] ?? '',
+                  onTap: () {
+                    print('Tapped on ${_filteredProducts[index]['title']}');
+                  },
+                );
+              },
+            ),
           ),
-        ),
-      ],
-    ),
-  );
+        ],
+      ),
+    );
   }
 }
