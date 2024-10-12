48decdd90af993c4b57ae9b3ac0a1fb600857e83
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-10-13 02:18:05 +0800

    Added product model, exception, and sample food data

---
+++ lib/data/food_data.dart
@@ -0,0 +1,34 @@
+import 'package:cs319_dart_playground/models/product.dart';
+
+List<Product> foodProducts = <Product>[
+  Product(
+    name: 'Adobo',
+    price: 100,
+    imageUrl: './images/Adobo.jpg',
+  ),
+  Product(
+    name: 'Sinigang na Baboy',
+    price: 200,
+    imageUrl: './images/sinigang-baboy.jpg',
+  ),
+  Product(
+    name: 'Menudo',
+    price: 300,
+    imageUrl: './images/menudo.jpg',
+  ),
+  Product(
+    name: 'Sisig',
+    price: 400,
+    imageUrl: './images/sisig.jpg',
+  ),
+  Product(
+    name: 'Kare-Kare',
+    price: 500,
+    imageUrl: './images/karekare.jpg',
+  ),
+  Product(
+    name: 'Lechon',
+    price: 600,
+    imageUrl: './images/lechon.jpg'
+  ),
+];
